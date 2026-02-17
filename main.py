b1db4756f799ae70af9595eab95ac1330aa2e967
Samkeliso Makedama <samkelisomakedama1315@gmail.com>
2026-02-17 16:09:47 +0200

    q

--- main.py
+++ main.py
@@ -59,3 +59,57 @@ get_db()
 @app.get("/")
 def root():
     return {"message": "Welcome to the User Management API!"}
+
+#endpoint to manage users
+
+#Get user by ID
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id:int, db:Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+#Create/add new user
+@app.post("/users/", response_model=UserResponse)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    if db.query(User).filter(User.email == user.email).first():
+        raise HTTPException(status_code=400, detail="User already registered")
+    new_user = User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+#update user information
+@app.put("/users/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, user: UserCreate, db:Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User does not exist")
+    
+    for field, value in user.dict().items():
+        setattr(db_user, field, value)
+
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+#Delete user
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User does not exist")
+    
+    db.delete(db_user)
+    db.commit()
+    #db.refresh(db_user)
+    return {"message": "User deleted successfully"}
+
+#get all users
+@app.get("/users/", response_model=List[UserResponse])
+def get_all_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
